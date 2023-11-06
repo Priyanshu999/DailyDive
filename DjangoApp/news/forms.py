dcd4fbaff4ccc1e3e3ab857cbb36e3732504dd36
sarcastic-yogi <ypatel7024@gmail.com>
2023-11-07 02:35:31 +0530

    Finish designing the signup page: Added responsive layout and styled form elements for improved user experience.

--- DjangoApp/news/forms.py
+++ DjangoApp/news/forms.py
@@ -4,19 +4,19 @@ from django.contrib.auth.models import User
 
 class CustomSignupForm(UserCreationForm):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'id': 'name'}),
+        widget=forms.TextInput(attrs={'id': 'name', 'placeholder': 'Username'}),
     )
 
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={'id': 'email'}),
+        widget=forms.EmailInput(attrs={'id': 'email', 'placeholder': 'Your Email'}),
     )
 
     password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'id': 'pass'}),
+        widget=forms.PasswordInput(attrs={'id': 'pass', 'placeholder': 'Password'}),
     )
 
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'id': 're_pass'}),
+        widget=forms.PasswordInput(attrs={'id': 're_pass', 'placeholder': 'Repeat your password'}),
     )
 
     class Meta:
