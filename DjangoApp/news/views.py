d12f9a49ec08e24359d8f899cc5a33685327896f
Priyanshu999 <spriyanshuprs@gmail.com>
2023-11-08 07:15:39 +0530

    Home page 80% complete, implemented article listing and pagination

--- DjangoApp/news/views.py
+++ DjangoApp/news/views.py
@@ -1,7 +1,10 @@
 from django.views.generic.edit import FormView
 from .forms import CustomSignupForm
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.db.models import F, Subquery, Window, Count, Max
 from django.contrib.auth import login
+from django.db.models.functions import Rank, DenseRank, TruncDate
+from .models import NewsArticle
 # from verify_email.email_handler import send_verification_email
 # from django.shortcuts import render, re
 # from django.views import View
@@ -20,6 +23,40 @@ class SignupView(FormView):
         return super().form_valid(form)
     
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = NewsArticle
     template_name = 'index.html'
+    # context_object_name = 'articles'
+    paginate_by = 14
 
+    def get_queryset(self):
+        # Create an initial queryset for articles
+        articles = NewsArticle.objects.all()
+
+        # Apply additional filtering based on your requirements
+        articles = self.mix_articles(articles)
+
+        # Slice the filtered queryset
+        articles = articles.order_by('-publication_date')[:300]
+
+        return articles
+
+
+    def mix_articles(self, articles):
+        # Group articles by the date part of publication_date and annotate the count
+        articles = articles.annotate(
+            publication_date_date=TruncDate('publication_date')
+        ).values('publication_date_date').annotate(
+            article_count=Count('id')
+        )
+
+        # Sort the grouped articles by count in descending order
+        articles = articles.order_by('-article_count')
+
+        # Extract the date parts from the grouped result
+        publication_date_dates = articles.values_list('publication_date_date', flat=True)
+
+        # Fetch and order the articles based on the sorted date parts
+        sorted_articles = NewsArticle.objects.filter(publication_date__date__in=publication_date_dates).order_by('-publication_date')
+        return sorted_articles
+    
